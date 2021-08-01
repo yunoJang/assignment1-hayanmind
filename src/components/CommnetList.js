22fce492f31fb4fa834dd89a71e5bde9d5e02c5e
yuno <hanganda23@naver.com>
2021-08-02 05:22:43 +0900

    feat : 스크롤 쓰로틀링 적용, 로딩 확인

--- src/components/CommnetList.js
+++ src/components/CommnetList.js
@@ -1,61 +1,53 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Comment from './Comment';
+import { throttle } from 'utils/delay';
 
-function CommnetList() { 
+function CommnetList() {
     const [comments, setComments] = useState([]);
     const [page, setPage] = useState(1);
     const [isLoading, setIsLoading] = useState(true);
-    const [isViewEnd, setIsViewEnd] = useState(false);
-  
-    useEffect(()=> {
-        if (isViewEnd) {
-            setPage(page=> page+1);
-            setIsViewEnd(false);
-        }
-    },[isViewEnd])
 
-    const onScroll = ()=> {
-        const {scrollHeight,clientHeight,scrollTop} = document.documentElement;
-        const degree =( scrollTop + clientHeight )/ scrollHeight;
+    const onScroll = () => {
+        const { scrollHeight, clientHeight, scrollTop } = document.documentElement;
+        const degree = (scrollTop + clientHeight) / scrollHeight;
 
-        if (degree >= 1) {
-            setIsViewEnd(true);
+        if (degree >= 0.95) {
+            setPage(page => page + 1);
         }
     }
 
-    useEffect(()=>{
-      window.addEventListener('scroll', onScroll, {passive:true});
+    useEffect(() => {
+        window.addEventListener('scroll', throttle(onScroll, 300), { passive: true });
+        return () => window.addEventListener('scroll', throttle(onScroll, 300), { passive: true });
+    }, []);
 
-      return () => window.removeEventListener('scroll', onScroll, {passive:true});
-    },[]);
-  
-    const loadComments = async page=> {
+    const loadComments = async page => {
         const requestURL = `https://jsonplaceholder.typicode.com/comments?_page=${page}&_limit=10`;
-        
+
         setIsLoading(true);
 
         try {
-            const {data : loadedComments} = await axios.get(requestURL);
-            
-            setComments(comments=> comments.concat(loadedComments));
+            const { data: loadedComments } = await axios.get(requestURL);
+
+            setComments(comments => comments.concat(loadedComments));
 
             setIsLoading(false);
         }
-        catch(err) {
+        catch (err) {
             console.error(err)
         }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         loadComments(page)
-    },[page])
+    }, [page])
 
-    const renderComments = ()=> {
-        return comments.map(comment=> 
-            <Comment 
-                key={comment.id} 
-                id={comment.id} 
+    const renderComments = () => {
+        return comments.map(comment =>
+            <Comment
+                key={comment.id}
+                id={comment.id}
                 email={comment.email}
                 body={comment.body}
             />
@@ -63,9 +55,12 @@ function CommnetList() {
     }
 
     return (
-        <ul className='commnet-list'>
-            {renderComments()}
-        </ul>
+        <>
+            <ul className='commnet-list'>
+                {renderComments()}
+            </ul>
+            {isLoading ? <div className='loading'>Loading...</div> : null}
+        </>
     )
 }
 
